9c6f4bfb1aa0d465c7997b3087c3e50582c107ca
Наталья Сидорова <nataha1654@gmail.com>
2025-02-16 17:35:08 +0300

    изменение классов Герой, Противник, Босс

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,4 +8,56 @@ internal static class Program
     {
         Console.WriteLine("Hello World!");
     }
+    public class Personage
+    {
+        public string Name { get; set; }
+        public uint Health { get; set; }
+        public uint CurrentHealth { get; set; }
+        public int Strong { get; set; }
+        public int Protection { get; set; }
+        public uint Level { get; set; }
+        public int MinAttack { get; set; }
+        public int MaxAttack { get; set; }
+
+        public Personage(string name, uint health, uint currentHealth, int strong, int protection, uint level, int minAttack, int maxAttack)
+        {
+            Name = name;
+            Health = health;
+            CurrentHealth = currentHealth;
+            Strong = strong;
+            Protection = protection;
+            Level = level;
+            MinAttack = minAttack;
+            MaxAttack = maxAttack;
+        }
+    }
+
+    public class Hero : Personage
+    {
+        public uint Exp { get; set; }
+
+        public Hero(string name, uint health, uint currentHealth, int strong, int protection, uint level, uint exp, int minAttack, int maxAttack)
+            : base(name, health, currentHealth, strong, protection, level, minAttack, maxAttack)
+        {
+            Exp = exp;
+        }
+    }
+
+    public class Opponent : Personage
+    {
+        public Opponent(string name, uint health, uint currentHealth, int strong, int protection, uint level, int minAttack, int maxAttack)
+            : base(name, health, currentHealth, strong, protection, level, minAttack, maxAttack)
+        {
+
+        }
+    }
+
+    public class Boss : Personage
+    {
+        public Boss(string name, uint health, uint currentHealth, int strong, int protection, uint level, int minAttack, int maxAttack)
+            : base(name, health, currentHealth, strong, protection, level, minAttack, maxAttack)
+        {
+
+        }
+    }
 }
